708e17f790f9259405b8358de9b53d00965c25bb
AungKyawPaing1989 <114481015+AungKyawPaing1989@users.noreply.github.com>
2022-10-04 16:57:13 +0630

    assignment2

---
+++ Assignment2.js
@@ -0,0 +1,29 @@
+const express = require ("express");
+const path = require ("path");
+const app = express();
+
+app.get("/page1",(req,res)=>{
+  
+  res.sendfile(path.join(_dirname+ "/page1.html")
+})
+
+app.get("/page2",(req,res)=>{
+  
+  res.sendfile(path.join(_dirname+ "/page2.html")
+})
+
+app.get("/page3",(req,res)=>{
+  
+  res.sendfile(path.join(_dirname+ "/page3.html")
+})
+
+app.get("/page4",(req,res)=>{
+  
+  res.sendfile(path.join(_dirname+ "/page4.html")
+})
+
+app.listen(3000,()=>{
+  
+ 
+   console.log('Server starting up:port:3000:);
+ })
